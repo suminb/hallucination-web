23ce8f8a986fe53fc13bac0e18660768602148f7
Sumin Byeon <suminb@gmail.com>
2013-11-12 01:40:51 -0700

    Deployment script.

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+HOST="sumin@suminb.com"
+
+# Delete .pyc files
+rm -rf $(find . -name "*.pyc")
+
+# Deploy files
+rsync -azP -e ssh * $HOST:webapps/hallucination/webapp
+
+read -p "Restart the server? " -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+	echo "Restarting the server..."
+	ssh $HOST 'webapps/hallucination/apache2/bin/restart'
+fi
